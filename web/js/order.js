828c0fa9595b04581dc8329ef2056642f855a372
dstb35 <agentedelfi@hotmail.com>
2020-10-26 15:06:31 +0100

    Commit 26-10-2020

--- web/js/order.js
+++ web/js/order.js
@@ -1,11 +1,5 @@
 $(document).ready(function () {
     restaurantid = $('#title').data('restaurantid');
-    console.log("document.URL : "+document.URL);
-    console.log("document.location.href : "+document.location.href);
-    console.log("document.location.origin : "+document.location.origin);
-    console.log("document.location.hostname : "+document.location.hostname);
-    console.log("document.location.host : "+document.location.host);
-    console.log("document.location.pathname : "+document.location.pathname);
 
     $.ajax({
         type: "GET",
